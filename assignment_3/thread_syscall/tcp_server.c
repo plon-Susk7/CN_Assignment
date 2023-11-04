172bce7dc59cb3747cb2dad52251fd3f94e5bd78
priyash7 <priyashshah4@gmail.com>
2023-11-04 22:16:24 +0530

    added more

--- assignment_3/thread_syscall/tcp_server.c
+++ assignment_3/thread_syscall/tcp_server.c
@@ -1,132 +1,91 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <inttypes.h>
-#include <pthread.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
 
-pthread_mutex_t output_mutex;
+#define PORT 8080
+#define MAX_CONNECTIONS 4000
 
-int client_count = 0;
+void *connection_handler(void *socket_desc) {
+    int client_socket = *(int *)socket_desc;
+    char client_message[2000];
+    ssize_t read_size;
 
-struct thread_args {
-    int client_fd;
-};
-
-long long int factorial(int n) {
-    if (n == 0) {
-        return 1;
-    }
-
-    if (n > 20) n = 20;
-
-    long long int result = 1;
-
-    for (int i = 1; i <= n; i++) {
-        result *= i;
-    }
-
-    return result;
-}
-
-void* thread_handle(void *args) {
-    struct thread_args *arg = (struct thread_args *)args;
-    int client_fd = arg->client_fd;
-    char buffer[1000];
-
-    client_count++; // Increment the client count
-    printf("Client connected (Client %d)\n", client_count);
-
-    while (1) {
-        bzero(buffer, sizeof(buffer));
-
-        ssize_t bytesRead = read(client_fd, buffer, sizeof(buffer));
-
-        if (bytesRead <= 0) {
-            if (bytesRead == 0) {
-                // Client closed the connection
-                printf("Client %d disconnected\n", client_count);
-            } else {
-                perror("Error reading from client");
-            }
-            break;
+    while ((read_size = recv(client_socket, client_message, sizeof(client_message), 0) > 0)) {
+        int n = atoi(client_message);
+        if (n > 20) {
+            n = 20;
         }
 
-        printf("Client %d sent: %s\n", client_count, buffer);
+        long long int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
 
-        int n = atoi(buffer);
-
-        long long int result = factorial(n);
-
-        char result_str[1000];
-
-        sprintf(result_str, "%lld", result);
-        printf("Writing to client %d: %s\n", client_count, result_str);
-
-        write(client_fd, result_str, strlen(result_str) + 1);
+        char response[2000];
+        sprintf(response, "Factorial of %d is %lld", n, result);
+        write(client_socket, response, strlen(response));
+        memset(client_message, 0, sizeof(client_message));
     }
 
-    close(client_fd);
-    free(args); // Free the thread arguments
+    if (read_size == 0) {
+        puts("Client disconnected");
+    } else if (read_size == -1) {
+        perror("recv failed");
+    }
 
-    pthread_exit(0);
+    free(socket_desc);
+    close(client_socket);
+    return NULL;
 }
 
 int main() {
-    pthread_t thread_id[10];
+    int server_socket, client_socket, c;
+    struct sockaddr_in server, client;
 
-    struct sockaddr_in server_addr;
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (server_fd < 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("Could not create socket");
+        return 1;
     }
 
-    int bindStatus = bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORT);
 
-    if (bindStatus < 0) {
+    if (bind(server_socket, (struct sockaddr *)&server, sizeof(server)) < 0) {
         perror("Bind failed");
-        exit(EXIT_FAILURE);
+        return 1;
     }
 
-    int listenStatus = listen(server_fd, 10);
+    listen(server_socket, MAX_CONNECTIONS);
+    puts("Server listening");
 
-    if (listenStatus < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
-    }
+    c = sizeof(struct sockaddr_in);
 
-    printf("Server listening on port 8080\n");
+    pthread_t thread_id;
 
-    for (int z = 0; z < 10; z++) {
-        struct sockaddr_in client_addr;
-        socklen_t client_addr_len = sizeof(client_addr);
-        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+    while ((client_socket = accept(server_socket, (struct sockaddr *)&client, (socklen_t *)&c))) {
+        int *new_sock = malloc(1);
+        *new_sock = client_socket;
 
-        if (client_fd < 0) {
-            perror("Accept failed");
-            continue; // Continue to the next iteration
+        if (pthread_create(&thread_id, NULL, connection_handler, (void *)new_sock) < 0) {
+            perror("Could not create thread");
+            return 1;
         }
 
-        struct thread_args *args = malloc(sizeof(struct thread_args));
-        args->client_fd = client_fd;
-        pthread_create(&thread_id[z], NULL, thread_handle, (void *)args);
+        pthread_detach(thread_id);
     }
 
-    for (int i = 0; i < 10; i++) {
-        pthread_join(thread_id[i], NULL);
+    if (client_socket < 0) {
+        perror("Accept failed");
+        return 1;
     }
 
-    close(server_fd);
+    close(server_socket);
 
     return 0;
 }
